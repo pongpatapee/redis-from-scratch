926a843686e7a175a72b01c4b2a03add3d82c3ca
pongpatapee <pongpatapee1021@gmail.com>
2024-11-08 17:28:38 -0500

    fix: can respond to multiple pings

--- app/server.go
+++ app/server.go
@@ -13,22 +13,41 @@ var (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
+	defer listener.Close()
+
+	fmt.Println("Server listening on port 6379")
+
 	for {
-		conn, err := l.Accept() // blocking call
+		conn, err := listener.Accept() // blocking call
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			// os.Exit(1)
+			continue
 		}
 
-		conn.Write([]byte("+PONG\r\n"))
+		handleClient(conn)
 	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	_, err := conn.Read(buf)
+	if err != nil {
+		return
+	}
+
+	// fmt.Println("Received Data:", string(buf[:n]))
+
+	// // return back the same data
+	// conn.Write(buf[:n])
+	conn.Write([]byte("+PONG\r\n"))
 }
