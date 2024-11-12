a2be1262ddc55326a7b56d05b89d03e4571302a3
pongpatapee <pongpatapee1021@gmail.com>
2024-11-12 00:30:13 -0500

    feat: init resp obj for serializing and deserializing RESP

---
+++ app/resp.go
@@ -0,0 +1,65 @@
+// Seralizing and Deserializing RESP input/output
+
+package main
+
+import (
+	"bufio"
+	"io"
+	"strconv"
+)
+
+const (
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
+)
+
+type Value struct {
+	typ   string // data type
+	str   string
+	num   int
+	bulk  string
+	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	// read until \r\n are the last two chracter
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+
+		n += 1
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+
+	}
+	return line[:len(line)-2], n, nil
+}
+
+func (r *Resp) readInteger() (x int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	x, err = strconv.Atoi(string(line))
+	if err != nil {
+		return 0, n, err
+	}
+
+	return x, n, nil
+}
